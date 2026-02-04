4aeb2e53ca20495ad3ebe9d147ebeb30abde32ba
Pinponik <256419204+Pinponik@users.noreply.github.com>
2026-02-04 21:29:36 +0100

    Add incremental build artifacts and output files
    - Created new binary files for query cache and work products in the incremental build directory.
    - Added lock file for the incremental build process.
    - Generated standard output and error files for the build process, indicating successful compilation of the `rankfide` project.

---
+++ target/debug/deps/rankfide-46f39df99ecab827.d
@@ -0,0 +1,5 @@
+C:\Users\n\Desktop\Prog\Rust\rankfide\target\debug\deps\rankfide-46f39df99ecab827.d: src\main.rs
+
+C:\Users\n\Desktop\Prog\Rust\rankfide\target\debug\deps\librankfide-46f39df99ecab827.rmeta: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/rankfide-54153cde75060a8b.d
@@ -0,0 +1,5 @@
+C:\Users\n\Desktop\Prog\Rust\rankfide\target\debug\deps\rankfide-54153cde75060a8b.d: src\main.rs
+
+C:\Users\n\Desktop\Prog\Rust\rankfide\target\debug\deps\librankfide-54153cde75060a8b.rmeta: src\main.rs
+
+src\main.rs:
