5552bb84afd1e2c8a3acf92d261ca1455c132bbd
Pinponik <256419204+Pinponik@users.noreply.github.com>
2026-02-08 18:33:33 +0100

    Add simple `Hello World` to test `egui`.

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,44 @@
 use eframe::egui;
+use eframe::App;
 use std::error::Error;
 use std::fs::File;
 use csv::ReaderBuilder;
 
+struct MyApp;
+
+impl App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello, World!");
+            ui.label("This is your first eframe app.");
+        });
+    }
+}
+
+
 struct ProbabilityRecord {
-    difference_from: u16,
-    difference_to: u16,
-    probability_when_me_bigger: u8,
-    probability_when_me_smaller: u8,
+    min_diff: u16,
+    max_diff: u16,
+    prob_big: u8,
+    prob_small: u8,
 }
 
 impl ProbabilityRecord {
     fn new() -> Self {
         Self {
-            difference_from: 0,
-            difference_to: 0,
-            probability_when_me_bigger: 0,
-            probability_when_me_smaller: 0,
+            min_diff: 0,
+            max_diff: 0,
+            prob_big: 0,
+            prob_small: 0,
         }
     }
     
-    fn new_from(difference_from: u16, difference_to: u16, probability_when_me_bigger: u8, probability_when_me_smaller: u8) -> Self {
+    fn new_from(min_diff: u16, max_diff: u16, prob_big: u8, prob_small: u8) -> Self {
         Self {
-            difference_from,
-            difference_to,
-            probability_when_me_bigger,
-            probability_when_me_smaller,
+            min_diff,
+            max_diff,
+            prob_big,
+            prob_small,
         }
     }
     
@@ -33,12 +46,12 @@ impl ProbabilityRecord {
         if record.len() != 4 {
             return Err("Invalid record length".into());
         }
-        Ok(Self {
-            difference_from: record[0].parse()?,
-            difference_to: record[1].parse()?,
-            probability_when_me_bigger: record[2].parse()?,
-            probability_when_me_smaller: record[3].parse()?,
-        })
+        Ok(Self::new_from(
+            record[0].parse()?,
+            record[1].parse()?,
+            record[2].parse()?,
+            record[3].parse()?,
+        ))
     }
 }
 
@@ -46,7 +59,7 @@ fn load_from_csv(file: &str) -> Result<Vec<ProbabilityRecord>, Box<dyn Error>> {
     let file = File::open(file)?;
     let mut rdr = ReaderBuilder::new()
         .delimiter(b',') 
-        .has_headers(false)
+        .has_headers(true)
         .from_reader(file);
 
     let mut records = Vec::new();
@@ -58,6 +71,14 @@ fn load_from_csv(file: &str) -> Result<Vec<ProbabilityRecord>, Box<dyn Error>> {
 }
 
 fn main() {
+    let native_options = eframe::NativeOptions::default();
+    let _ =
+    eframe::run_native(
+        "Hello World",
+        native_options,
+        Box::new(|_cc| Box::new(MyApp)),
+    );
+
     let probability = load_from_csv(r"src\table.csv").unwrap();
 
 }
